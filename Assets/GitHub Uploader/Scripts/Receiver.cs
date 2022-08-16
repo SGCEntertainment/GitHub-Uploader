ce74cfc655c3ff1b2000ac3456a5ebc2ca9544bc
nikcxprojects@gmail.com <smallgamescompany@gmail.com>
2022-08-16 13:17:43 +0300

    update

---
+++ Assets/GitHub Uploader/Scripts/Receiver.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Receiver : MonoBehaviour
+{
+    IEnumerator DownloadFile(string objectUrl)
+    {
+        var uwr = new UnityWebRequest(objectUrl, UnityWebRequest.kHttpVerbGET);
+        string path = Path.Combine(Application.persistentDataPath, "test_project.zip");
+        uwr.downloadHandler = new DownloadHandlerFile(path);
+
+        yield return uwr.SendWebRequest();
+
+        if (uwr.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(uwr.error);
+        }
+        else
+        {
+            Debug.Log("File successfully downloaded and saved to " + path);
+        }
+    }
+
+    public void DownloadProject(string objectUrl)
+    {
+        StartCoroutine(DownloadFile(objectUrl));
+    }
+}
